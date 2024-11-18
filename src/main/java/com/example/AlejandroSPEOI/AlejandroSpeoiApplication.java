98b29bbfbe07c9632a6ee552495a8c367e93cf65
AlejandroASP <alejandroasp71@gmail.com>
2024-11-18 16:19:04 +0000

    New(añadir proyecto)
    Se añade el proyecto con las respectivas dependencias

---
+++ src/main/java/com/example/AlejandroSPEOI/AlejandroSpeoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlejandroSPEOI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlejandroSpeoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlejandroSpeoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AlejandroSPEOI/AlejandroSpeoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlejandroSPEOI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlejandroSpeoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
